9a0192381d08463194593b800c7df13edbf87d00
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-18 16:55:35 +0530

    adding customer service module to the application and also adding yml file and main class. closes #3

---
+++ customer-service/src/main/java/com/luv4code/customer/app/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.customer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
