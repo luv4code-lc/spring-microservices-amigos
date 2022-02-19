3e95efd3b0781b097cb58aba1ad0e436a20ab848
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-19 17:36:06 +0530

    adding feign client for fraudcheck in clients module. closes #20

---
+++ clients/src/main/java/com/luv4code/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,14 @@
+package com.luv4code.clients.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class FraudCheckResponse {
+    private Boolean isFraudster;
+
+    public Boolean isFraudster() {
+        return false;
+    }
+}
