e9f80a51fb65117e3f85e3ffb16c83e59fc1bbee
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-20 12:16:41 +0530

    adding amqp configurations in notifaication service. closes #31

--- notification-service/src/main/java/com/luv4code/service/notification/app/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/luv4code/service/notification/app/NotificationServiceApplication.java
@@ -1,15 +1,35 @@
 package com.luv4code.service.notification.app;
 
+import com.luv4code.amqp.RabbitMQMessageProducer;
+import com.luv4code.service.notification.app.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.luv4code.service.notification.app",
+        "com.luv4code.amqp"
+})
 @EnableEurekaClient
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
+
+    /*@Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig
+    ) {
+        return args -> {
+            producer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }*/
 
 }
