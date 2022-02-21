687d164414101d7201a9244684f2d202392c840a
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-21 10:39:19 +0530

    adding rabbit mq cosumer. closes #33

---
+++ notification-service/src/main/java/com/luv4code/service/notification/app/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,23 @@
+package com.luv4code.service.notification.app.rabbitmq;
+
+import com.luv4code.clients.notification.NotificationRequest;
+import com.luv4code.service.notification.app.service.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.send(notificationRequest);
+    }
+
+}
